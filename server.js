63583c13ef260a1ad5fb95d07d3e18fc98a3ea4b
faisal09alam <faisal.009alam@gmail.com>
2022-01-12 00:31:03 +0530

    little change.

--- server.js
+++ server.js
@@ -23,5 +23,5 @@ require("./app/router/router")(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
+  // console.log(`Server is running on port ${PORT}.`);
 });
