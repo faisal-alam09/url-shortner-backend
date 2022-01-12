bc0a47c37f2bb0b9ab94b81b58316235652d6a1e
faisal09alam <faisal.009alam@gmail.com>
2022-01-12 13:39:55 +0530

    added CorsPolicy for frontend react app.

--- app/config.js
+++ app/config.js
@@ -4,7 +4,8 @@ var config = {};
 // config.mongodbString = "mongodb://localhost:27017/test";
 config.mongodbString = "mongodb+srv://faisalalam7861:faisalalam009@cluster0.aesrk.mongodb.net/test" //mongodb Atlas live
 config.dbCollection  = "test_collection";
-config.CorsOption    = "http://localhost:3000"; //Cors Option
+// config.CorsOption    = "http://localhost:3000"; //Cors Option
+config.CorsOption    = "https://url-shortner-faisal09.herokuapp.com/";
 
 module.exports = config;
 
