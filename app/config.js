8127070091ee5b3de1c3a9b7f639522115b24f82
faisal09alam <faisal.009alam@gmail.com>
2022-02-02 21:41:15 +0530

    Added Config file

--- app/config.js
+++ app/config.js
@@ -2,7 +2,7 @@
 var config = {};
 
 // config.mongodbString = "mongodb://localhost:27017/test";
-config.mongodbString = "mongodb+srv://faisalalam7861:faisalalam009@cluster0.aesrk.mongodb.net/test" //mongodb Atlas live
+config.mongodbString = "mongodb+srv://faisalalam7861:<PASSWORD9>@cluster0.aesrk.mongodb.net/test" //mongodb Atlas live
 config.dbCollection  = "test_collection";
 // config.CorsOption    = "http://localhost:3000"; //Cors Option
 config.CorsOption    = "*";
